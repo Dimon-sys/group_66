333254459347acba693ddbddc86b803bfcd9ec38
Dimon-sys <136633867+Dimon-sys@users.noreply.github.com>
2023-08-17 12:24:56 +0300

    Add files via upload

---
+++ advertisements_66/app_advertisements_66/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class AppAdvertisements66Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'app_advertisements_66'
+    verbose_name = 'Объявления'
